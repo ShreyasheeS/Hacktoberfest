ccb901d9d02ac8af5a01222f38a0e391c57d7f82
sherilpaulin <sherilchristopher@Sherils-MacBook-Pro.local>
2017-10-21 14:35:35 -0400

    added hello world function in java that displays it in a java application

---
+++ hello_world/Java/HelloWorldJavaApp.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame; 
+import javax.swing.JLabel; 
+/**
+ * Prints hello world on a Java application
+ * @author sherilpaulin
+ *
+ */
+public class HelloWorldJavaApp {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();           
+        frame.setTitle("Hi!");                
+        frame.add(new JLabel("Hello, world!"));
+        frame.pack();                     
+        frame.setLocationRelativeTo(null);    
+        frame.setVisible(true);        
+    }
+}
